2c1b6b7925126588711a8c31206f084f130f0e61
L3gendElias <jebarragan2021@gmail.com>
2025-09-09 20:38:22 -0600

    Security commit, lot of changes and I do not wanna risk

---
+++ Code/JavaScript/catalog.js
@@ -0,0 +1,11 @@
+function miniTakis(){
+    window.location.href = "minitakis.html";
+};
+
+function CacahuatesG(){
+    window.location.href = "cacahuatesg.html";
+}
+
+function CacahuatesS(){
+    window.location.href = "cacahuatess.html";
+}
